d2da861a362dd7d7a963572bdf942b5ebdf6cf5c
Wesleyel <48174882+wesleyel@users.noreply.github.com>
2024-11-14 07:11:49 +0000

    feat: remove tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
