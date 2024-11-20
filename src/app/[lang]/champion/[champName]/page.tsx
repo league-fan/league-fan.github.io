677a0026e8a9ba49bc7689a049d71e7f037bcf67
magicwenli <48174882+magicwenli@users.noreply.github.com>
2024-11-20 20:15:11 +0800

    feat: refactor champion page handling, implement new helper functions, and update context usage

---
+++ src/app/[lang]/champion/[champName]/page.tsx
@@ -0,0 +1,24 @@
+import { getChampionSkinsById, getChampionByName } from "@/data/helpers";
+import { useContext } from "react";
+import { PropsContext } from "../../props";
+
+// export async function generateStaticParams() {
+//     const { champions, lang } = useContext(PropsContext);
+//     return champions.map((champ) => ({
+//         lang,
+//         champName: champ.alias
+//     }))
+// }
+
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ champName: string }>
+}) {
+    const champName = (await params).champName
+    const { champions, skins } = useContext(PropsContext);
+    const champ = getChampionByName(champName, champions);
+    if (!champ) return <div>Champion not found</div>
+    const champSkin = getChampionSkinsById(champ.id, skins);
+    return <div>{JSON.stringify(champSkin, null, 2)}</div>
+}
